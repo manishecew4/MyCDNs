c0d40cca678850c05e7f6a5923a999dd69720003
Manish <manish.ecew4@gmail.com>
2021-11-15 19:09:49 +0530

    responsive

--- js/custom.js
+++ js/custom.js
@@ -65,28 +65,34 @@ $(document).ready(function () {
         infinite: true,
         swipe: true,
         swipeToSlide: false,
-        responsive: [{
-
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 4,
-                infinite: true
-            }
-
-        }, {
-
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
                 dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
             }
-
-        }, {
-
-            breakpoint: 300,
-            settings: "unslick" // destroys slick
-
-        }]
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 });
 
